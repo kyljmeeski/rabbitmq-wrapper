1a2a3533efb4b5f0f46736d2369fc6f71702817d
kyljmeeski <kyljmeeski@gmail.com>
2024-04-24 17:26:31 +0600

    feat: create Queues to handel queues

---
+++ src/main/java/com/kyljmeeski/rabbitmqwrapper/Queues.java
@@ -0,0 +1,25 @@
+package com.kyljmeeski.rabbitmqwrapper;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+public class Queues {
+
+    private final ConnectionFactory factory;
+
+    public Queues(ConnectionFactory factory) {
+        this.factory = factory;
+    }
+
+    public void declare(String name, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> args) throws IOException, TimeoutException {
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(name, durable, exclusive, autoDelete, args);
+    }
+
+}
