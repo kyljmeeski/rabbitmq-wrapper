d1ef52c663c70e98e3cc30da3a312a20d470564a
kyljmeeski <kyljmeeski@gmail.com>
2024-04-29 21:09:56 +0600

    doc: add javadoc to Consumer and PlainConsumer

--- src/main/java/com/kyljmeeski/rabbitmqwrapper/Consumer.java
+++ src/main/java/com/kyljmeeski/rabbitmqwrapper/Consumer.java
@@ -3,8 +3,17 @@ package com.kyljmeeski.rabbitmqwrapper;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * A functional interface for consuming messages from a RabbitMQ queue.
+ */
 public interface Consumer {
 
+    /**
+     * Starts consuming messages from the queue.
+     *
+     * @throws IOException      if an error occurs while communicating with RabbitMQ
+     * @throws TimeoutException if the operation times out while waiting for a response from RabbitMQ
+     */
     void startConsuming() throws IOException, TimeoutException;
 
 }
--- src/main/java/com/kyljmeeski/rabbitmqwrapper/PlainConsumer.java
+++ src/main/java/com/kyljmeeski/rabbitmqwrapper/PlainConsumer.java
@@ -8,18 +8,35 @@ import com.rabbitmq.client.DeliverCallback;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * A plain implementation of the {@link Consumer} interface that consumes messages from a RabbitMQ queue.
+ */
 public class PlainConsumer implements Consumer {
 
     private final ConnectionFactory factory;
     private final RabbitQueue queue;
     private final java.util.function.Consumer<String> consumer;
 
+    /**
+     * Constructs a {@code PlainConsumer} with the specified RabbitMQ connection factory,
+     * queue, and message consumer.
+     *
+     * @param factory  the RabbitMQ connection factory used to create connections
+     * @param queue    the RabbitMQ queue to consume messages from
+     * @param consumer the consumer function that processes received messages
+     */
     public PlainConsumer(ConnectionFactory factory, RabbitQueue queue, java.util.function.Consumer<String> consumer) {
         this.factory = factory;
         this.queue = queue;
         this.consumer = consumer;
     }
 
+    /**
+     * Starts consuming messages from the specified queue.
+     *
+     * @throws IOException      if an error occurs while communicating with RabbitMQ
+     * @throws TimeoutException if the operation times out while waiting for a response from RabbitMQ
+     */
     @Override
     public void startConsuming() throws IOException, TimeoutException {
         Connection connection = factory.newConnection();
