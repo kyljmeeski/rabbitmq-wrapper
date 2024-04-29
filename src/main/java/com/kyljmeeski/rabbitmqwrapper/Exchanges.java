504654a00664a2cb0c1984783df0678b757f5972
kyljmeeski <kyljmeeski@gmail.com>
2024-04-29 21:32:32 +0600

    doc: add javadoc to Exchanges, RabbitExchange

--- src/main/java/com/kyljmeeski/rabbitmqwrapper/Exchanges.java
+++ src/main/java/com/kyljmeeski/rabbitmqwrapper/Exchanges.java
@@ -7,14 +7,31 @@ import com.rabbitmq.client.ConnectionFactory;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * Utility class for declaring RabbitMQ exchanges.
+ */
 public class Exchanges {
 
     private final ConnectionFactory factory;
 
+    /**
+     * Constructs an {@code Exchanges} utility with the specified RabbitMQ connection factory.
+     *
+     * @param factory the RabbitMQ connection factory used to create connections
+     */
     public Exchanges(ConnectionFactory factory) {
         this.factory = factory;
     }
 
+    /**
+     * Declares a RabbitMQ exchange with the given name and type.
+     *
+     * @param name the name of the exchange
+     * @param type the type of the exchange (e.g., "direct", "topic", "fanout", etc.)
+     * @return a {@link RabbitExchange} object representing the declared exchange
+     * @throws IOException      if an error occurs while communicating with RabbitMQ
+     * @throws TimeoutException if the operation times out while waiting for a response from RabbitMQ
+     */
     public RabbitExchange declare(String name, String type) throws IOException, TimeoutException {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
--- src/main/java/com/kyljmeeski/rabbitmqwrapper/RabbitExchange.java
+++ src/main/java/com/kyljmeeski/rabbitmqwrapper/RabbitExchange.java
@@ -1,13 +1,27 @@
 package com.kyljmeeski.rabbitmqwrapper;
 
+/**
+ * Represents a RabbitMQ exchange.
+ */
 public class RabbitExchange {
+
 
     private final String name;
 
+    /**
+     * Constructs a {@code RabbitExchange} with the specified exchange name.
+     *
+     * @param name the name of the RabbitMQ exchange
+     */
     public RabbitExchange(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the name of the RabbitMQ exchange.
+     *
+     * @return the name of the exchange
+     */
     public String name() {
         return name;
     }
