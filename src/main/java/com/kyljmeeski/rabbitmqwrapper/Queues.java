a3a5fccd1884cb1716160e34154ef48d6ec2709b
kyljmeeski <kyljmeeski@gmail.com>
2024-04-29 21:49:19 +0600

    doc: add javadoc to Queues, RabbitQueue

--- src/main/java/com/kyljmeeski/rabbitmqwrapper/Queues.java
+++ src/main/java/com/kyljmeeski/rabbitmqwrapper/Queues.java
@@ -8,14 +8,34 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * Utility class for declaring RabbitMQ queues.
+ */
 public class Queues {
 
     private final ConnectionFactory factory;
 
+    /**
+     * Constructs a {@code Queues} utility with the specified RabbitMQ connection factory.
+     *
+     * @param factory the RabbitMQ connection factory used to create connections
+     */
     public Queues(ConnectionFactory factory) {
         this.factory = factory;
     }
 
+    /**
+     * Declares a RabbitMQ queue with the specified parameters.
+     *
+     * @param name       the name of the queue
+     * @param durable    {@code true} if the queue should survive a server restart, {@code false} otherwise
+     * @param exclusive  {@code true} if the queue should be used by only one connection, {@code false} otherwise
+     * @param autoDelete {@code true} if the queue should be deleted when there are no more consumers, {@code false} otherwise
+     * @param args       additional arguments used for configuring the queue (can be {@code null})
+     * @return a {@link RabbitQueue} object representing the declared queue
+     * @throws IOException      if an error occurs while communicating with RabbitMQ
+     * @throws TimeoutException if the operation times out while waiting for a response from RabbitMQ
+     */
     public RabbitQueue declare(String name, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> args) throws IOException, TimeoutException {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
--- src/main/java/com/kyljmeeski/rabbitmqwrapper/RabbitQueue.java
+++ src/main/java/com/kyljmeeski/rabbitmqwrapper/RabbitQueue.java
@@ -7,20 +7,43 @@ import com.rabbitmq.client.ConnectionFactory;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * Represents a RabbitMQ queue.
+ */
 public class RabbitQueue {
 
     private final ConnectionFactory factory;
     private final String name;
 
+    /**
+     * Constructs a {@code RabbitQueue} with the specified RabbitMQ connection factory and queue name.
+     *
+     * @param factory the RabbitMQ connection factory used to create connections
+     * @param name    the name of the RabbitMQ queue
+     */
     public RabbitQueue(ConnectionFactory factory, String name) {
         this.factory = factory;
         this.name = name;
     }
 
+    /**
+     * Gets the name of the RabbitMQ queue.
+     *
+     * @return the name of the queue
+     */
     public String name() {
         return name;
     }
 
+    /**
+     * Binds the queue to the specified exchange with the given routing key.
+     *
+     * @param exchange   the RabbitMQ exchange to bind to
+     * @param routingKey the routing key for the binding
+     * @return this {@code RabbitQueue} object after binding
+     * @throws IOException      if an error occurs while communicating with RabbitMQ
+     * @throws TimeoutException if the operation times out while waiting for a response from RabbitMQ
+     */
     public RabbitQueue bind(RabbitExchange exchange, String routingKey) throws IOException, TimeoutException {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
